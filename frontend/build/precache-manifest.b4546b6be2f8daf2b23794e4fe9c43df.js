e44c4d45eee455bb6f0861044808b7df22a5e4ba
soureshmondal <117149085+soureshmondal@users.noreply.github.com>
2025-01-26 23:46:27 +0530

    Add files via upload

---
+++ frontend/build/precache-manifest.b4546b6be2f8daf2b23794e4fe9c43df.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc6841215f6059c0fbc87861185b08c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "804b8fe8156679d41736",
+    "url": "/static/css/main.74220f98.chunk.css"
+  },
+  {
+    "revision": "a7482ef56307f994899c",
+    "url": "/static/js/2.e10c0eb6.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.e10c0eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e01894907aa8d016422",
+    "url": "/static/js/3.2fc5c290.chunk.js"
+  },
+  {
+    "revision": "804b8fe8156679d41736",
+    "url": "/static/js/main.1cf23275.chunk.js"
+  },
+  {
+    "revision": "300e7b5cb5f3c3157adb",
+    "url": "/static/js/runtime-main.f6613eaf.js"
+  },
+  {
+    "revision": "6c7ed4a9886e657190ea73b8e6cf41f2",
+    "url": "/static/media/image4.6c7ed4a9.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
